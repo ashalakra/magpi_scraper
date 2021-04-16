1d1591d0ccb29d99ae19f2f0defe5bd4963fec4f
Angelo Lakra <angelo.lakra@itron.com>
2021-04-16 17:35:47 -0600

    Adding some comments about lists and threads

--- magpi_scraper.py
+++ magpi_scraper.py
@@ -13,7 +13,20 @@ def get_page_urls():
     soup = BeautifulSoup(res.text,'html.parser')
     # Figure out number of pages needed to be scraped
     pagination_label = soup.select('.c-pagination__label')[0].get_text()
+
+    # COMMENT:
+    #
+    # You could call `object.split()[-1]`. This always gets the last
+    # object in a list, but if the list is empty then you will get an
+    # `IndexError`.  The same goes for `object.split()[2]` if the list
+    # is empty.
+    #
+    # For a small project like this, what you have is fine, but if
+    # it's a list that can change in size everytime you get it, then
+    # you might want to check the size of the list before trying to
+    # extract from a position.
     num_of_pages = int(pagination_label.split(' ')[2])
+
     for i in range(1, num_of_pages+1):
         URL = f"https://magpi.raspberrypi.org/issues?page={i}"
         page_urls.append(URL)
@@ -59,6 +72,19 @@ def downloadMagPi(URL):
 get_page_urls()
 
 # Begin threading process
+
+# COMMENT:
+#
+# If I understand this correctly, are you creating a thread for each
+# page and then having each thread download all the pdfs on its
+# particular page?
+#
+# This works, but if there are a lot of pages then you will spawn a
+# lot of threads. You have to be careful you don't create a denial of
+# service (DOS) attack unintentionally to a website.
+#
+# Sometimes a simple loop on the main thread works just fine and maybe
+# even faster depending on your computer and the website.
 for url in page_urls:
     processThread = threading.Thread(
         target=downloadMagPi, args=(url,))
